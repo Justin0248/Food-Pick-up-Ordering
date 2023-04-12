a1e0138881a06099ce57e38774099068d07ea6eb
Yonin408 <justin12689@gmail.com>
2023-04-12 15:52:33 -0700

    removed unnecessary columns

--- db/schema/01_food_orders.sql
+++ db/schema/01_food_orders.sql
@@ -9,16 +9,12 @@ CREATE TABLE users (
 );
 
 CREATE TABLE menu (
-    id SERIAL PRIMARY KEY NOT NULL,
-    restaurant_id INTEGER REFERENCES restaurant(id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
     category VARCHAR(255) NOT NULL,
     price INTEGER
 );
 
 CREATE TABLE restaurant (
-    id SERIAL PRIMARY KEY NOT NULL,
-    menu_id INTEGER REFERENCES menu(id) ON DELETE CASCADE,
     orders_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
@@ -28,11 +24,6 @@ CREATE TABLE restaurant (
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY NOT NULL,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    restaurant_id INTEGER REFERENCES restaurant(id) ON DELETE CASCADE,
-    menu_id INTEGER REFERENCES menu(id) ON DELETE CASCADE,
-    completion_time TIME NOT NULL,
-    incompleted_orders VARCHAR(255),
-    completed_orders VARCHAR(255),
-    order_status VARCHAR(255) NOT NULL
+    items TEXT[]
 );
 
