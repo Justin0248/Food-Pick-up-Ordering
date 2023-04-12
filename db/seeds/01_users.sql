a131cc304b800bae72dca04e803126222d2cd63e
NikJensen97 <npjjensen@hotmail.com>
2023-04-12 15:24:39 -0700

    added data to fit our table format

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password, phone, street_address) VALUES ('Alice', 'alice@gmail.com', 'password' , 6047220944, '123 Lighthouse Street');
+INSERT INTO users (name, email, password, phone, street_address) VALUES ('Kira', 'kirafoodlover.gmail.com', 'password', 123456789, '909 Triple lane');
