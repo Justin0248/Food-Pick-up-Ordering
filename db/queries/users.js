fa34249645f9453bbeaf9aa95e96c53ee54d0a3b
NikJensen97 <npjjensen@hotmail.com>
2023-04-12 16:48:28 -0700

    added remove item function

--- db/queries/users.js
+++ db/queries/users.js
@@ -23,12 +23,25 @@ const getUserWithPassword = (password) => {
 
 const addItemsToOrder = (item) => {
   return db.query('INSERT INTO orders (items) VALUES ($1);')
+  .then(data => {
+    return;
+  });
 };
-const calculateTotalPrice = (order) => {
+const removeItemsFromOrder = (item) => {
+  return db.query('DELETE FROM orders WHERE items = $1;')
+  .then(data => {
+    return;
+  });
 
+};
+const calculateTotalPrice = () => {
+  return db.query('SELECT sum(menu.price), FROM orders JOIN menu ON orders.items = menu.name, GROUP BY menu.price;')
+  .then(data => {
+    return data.rows;
+  });
 };
 const acceptOrderAndGiveTime = () => {
 
 };
 
-module.exports = { getUsers, getUserWithEmail, getUserWithPassword, addItemsToOrder };
+module.exports = { getUsers, getUserWithEmail, getUserWithPassword, addItemsToOrder, removeItemsFromOrder, calculateTotalPrice };
