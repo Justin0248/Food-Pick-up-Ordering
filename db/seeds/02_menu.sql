5612cf272b541f20efbd62d6f9b0507088c8fc22
NikJensen97 <npjjensen@hotmail.com>
2023-04-12 15:24:51 -0700

    seeded data for menu

---
+++ db/seeds/02_menu.sql
@@ -0,0 +1,4 @@
+INSERT INTO menu (name, catagory, price) VALUES ('Burger', 'main', 8);
+INSERT INTO menu (name, catagory, price) VALUES ('2L pop', 'drink', 3);
+INSERT INTO menu (name, catagory, price) VALUES ('Fries', 'side', 4);
+INSERT INTO menu (name, catagory, price) VALUES ('Onion rings', 'side', 4);
