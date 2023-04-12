798c04870d4a415d9899c59fe46c6dd3664309a6
Yonin408 <justin12689@gmail.com>
2023-04-12 16:08:45 -0700

    fix orders insertion

--- db/seeds/04_order.sql
+++ db/seeds/04_order.sql
@@ -1,1 +1,2 @@
-INSERT INTO orders (items) VALUES ('{"Burger", "Fries", "2L pop"}');
+INSERT INTO orders (user_id, items) VALUES (1, '{"Burger", "Fries", "2L pop"}');
+INSERT INTO orders (user_id, items) VALUES (2, '{"Onion rings", "Fries", "2L pop"}');
