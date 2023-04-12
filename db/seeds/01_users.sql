57bea474f68062d03af1dc877633f63b07c5d628
Yonin408 <justin12689@gmail.com>
2023-04-12 15:53:07 -0700

    table insertions

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,11 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name) VALUES ('Alice');
 INSERT INTO users (name) VALUES ('Kira');
+
+
+INSERT INTO restaurant (name) VALUES ('McD');
+INSERT INTO restaurant (email) VALUES ('RealMcD@email.com');
+INSERT INTO restaurant (password) VALUES ('123321');
+
+
+INSERT INTO orders (user_id, estimated_time) VALUES ('John Doe', TIME '00:30:00');
