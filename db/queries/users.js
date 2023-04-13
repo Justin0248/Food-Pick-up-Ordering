ee343d08083b0820056e220e7e59dbd7fef51a19
NikJensen97 <npjjensen@hotmail.com>
2023-04-12 17:02:47 -0700

    added delete function

--- db/queries/users.js
+++ db/queries/users.js
@@ -21,46 +21,21 @@ const getUserWithPassword = (password) => {
   });
 };
 
-<<<<<<< HEAD
-const addItemsToOrder = (item) => {
-  return db.query('INSERT INTO orders (items) VALUES ($1);')
-  .then(data => {
-    return;
-  });
-};
-const removeItemsFromOrder = (item) => {
-  return db.query('DELETE FROM orders WHERE items = $1;')
-  .then(data => {
-    return;
-  });
-=======
 const addItemsToOrder = (user_id, item) => {
   return db.query('INSERT INTO orders (items) VALUES ($1, $2);', [user_id, item])
 };
->>>>>>> 78de1b9f6886f217dc8f8840176e543af89f76f2
-
-const calculateTotalPrice = (item) => {
-  return db.query('SELECT SUM(price) FROM menu WHERE name = $1;', [item])
-  .then(data => {
-    return data.rows;
-  });
+const removeItemFromOrder = (item) => {
+  return db.query('DELETE FROM orders WHERE items = 1$;', [item])
 };
-<<<<<<< HEAD
+
 const calculateTotalPrice = () => {
   return db.query('SELECT sum(menu.price), FROM orders JOIN menu ON orders.items = menu.name, GROUP BY menu.price;')
   .then(data => {
     return data.rows;
   });
 };
-=======
-
->>>>>>> 78de1b9f6886f217dc8f8840176e543af89f76f2
 const acceptOrderAndGiveTime = () => {
 
 };
 
-<<<<<<< HEAD
-module.exports = { getUsers, getUserWithEmail, getUserWithPassword, addItemsToOrder, removeItemsFromOrder, calculateTotalPrice };
-=======
-module.exports = { getUsers, getUserWithEmail, getUserWithPassword, addItemsToOrder, calculateTotalPrice };
->>>>>>> 78de1b9f6886f217dc8f8840176e543af89f76f2
+module.exports = { getUsers, getUserWithEmail, getUserWithPassword, addItemsToOrder, calculateTotalPrice, removeItemFromOrder };
