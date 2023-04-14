c846f1248cf79833c2a3faa945b92a48f0d63c4f
NikJensen97 <npjjensen@hotmail.com>
2023-04-14 11:40:37 -0700

    removed my phone #

--- routes/twilioPosts.js
+++ routes/twilioPosts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 const accountSid = "ACf41b276b7ac4b193b6f865325dfc7b97";
-const authToken = '1507dfc261b5dbe0acbc57306755f119';
+const authToken = 'e4e564c88b38f023e07c484d11271a13';
 const client = require("twilio")(accountSid, authToken);
 
 router.post('/checkout', (req, res) => {
@@ -16,7 +16,8 @@ router.post('/checkout', (req, res) => {
 
 router.post('/order', (req, res) => {
   client.messages
-  .create({ body: "Your order will be ready in 20 minutes!", from: "+14344784631", to: "+16047220944" })
+  // phone # hidden/ to be added
+  .create({ body: "Your order will be ready in 20 minutes!", from: "+14344784631", to: "" })
     .then(message => console.log(message.sid));
   
 
